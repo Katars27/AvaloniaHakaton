caa3d2e0995c2928e9d2df9855c6e1e986fa674b
Licedej <ka1arsis27@gmail.com>
2025-01-24 11:19:03 +0300

    version 1.1 Улучшенный дизайн

--- AvaloniaProject/Views/MainWindow.axaml.cs
+++ AvaloniaProject/Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Threading;
 using AvaloniaProject.ViewModels;
+using System;
 
 namespace AvaloniaProject.Views
 {
@@ -9,18 +12,55 @@ namespace AvaloniaProject.Views
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
+
+            this.Opacity = 0; // Прозрачность
+            this.RenderTransform = new ScaleTransform(0.5, 0.5); 
+
+            // Анимация появления
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(16) 
+            };
+
+            double opacityStep = 0.05; // Скорость увеличения прозрачности
+            double scaleStep = 0.01; // Скорость увеличения масштаба
+
+            timer.Tick += (sender, args) =>
+            {
+                if (this.Opacity < 1)
+                {
+                    this.Opacity += opacityStep;
+                }
+
+                if (this.RenderTransform is ScaleTransform scale)
+                {
+                    if (scale.ScaleX < 1)
+                    {
+                        scale.ScaleX += scaleStep;
+                        scale.ScaleY += scaleStep;
+                    }
+                }
+
+                if (this.Opacity >= 1 && (this.RenderTransform as ScaleTransform)?.ScaleX >= 1)
+                {
+                    timer.Stop();
+                }
+            };
+
+            timer.Start();
         }
+
         private void OnCompressClick(object? sender, RoutedEventArgs e)
         {
             var vm = DataContext as MainWindowViewModel;
             vm?.Compress();
-            StatusText.Text = vm?.StatusMessage;
         }
+
         private void OnDecompressClick(object? sender, RoutedEventArgs e)
         {
             var vm = DataContext as MainWindowViewModel;
             vm?.Decompress();
-            StatusText.Text = vm?.StatusMessage;
         }
     }
 }
